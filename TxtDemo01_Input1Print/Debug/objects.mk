749c6ceb0e52b4661be79aba6f52dc717b79a789
Alexander Steiger <alexander.steiger@fischer.de>
2018-09-06 10:50:46 +0200

    clean up

--- TxtDemo01_Input1Print/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lKeLibTxt -lMotorIOLib -lROBOProLib -lSDLWidgetsLib -lTxtControlLib -lSDL -lSDL_gfx -lSDL_ttf -lts -lfreetype -lz -lpng16 -lbz2 -ljpeg -lasound -ldirectfb -lfusion -ldirect -ltiff -lSDL_image
-
