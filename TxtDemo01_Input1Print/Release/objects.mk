66779495935b72f96d1b3ccb0f7d5ad7257882b2
Carel van Leeuwen <C.vanLeeuwen@alumnus.utwente.nl>
2020-05-14 21:19:55 +0200

    CLean up the structure

--- TxtDemo01_Input1Print/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lKeLibTxt -lMotorIOLib -lROBOProLib -lSDLWidgetsLib -lTxtControlLib -lSDL -lSDL_gfx -lSDL_ttf -lSDL_image -lts -lfreetype -lz -lpng16 -lbz2 -ljpeg -lasound -ldirectfb -lfusion -ldirect -ltiff
-
